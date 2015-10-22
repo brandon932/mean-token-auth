e492d7958a30b5ad839c2dfa822a8d78ff64e19b
brandon932 <bthornton_932@yahoo.com>
2015-10-22 16:45:16 -0600

    added logout functionality

--- client/controllers/navbar.js
+++ client/controllers/navbar.js
@@ -1,12 +1,11 @@
 app.controller('navbarCtrl', function($scope, $rootScope, $window, $auth) {
 
-  $scope.isAuthenticated = function() {
-    return $auth.isAuthenticated();
-  };
+    $scope.isAuthenticated = function() {
+        return $auth.isAuthenticated();
+    };
 
-  $scope.logout = function() {
-    $auth.logout();
-    delete $window.localStorage.currentUser;
-  };
-
+    $scope.logout = function() {
+        $auth.logout();
+        delete $window.localStorage.currentUser;
+    };
 });
